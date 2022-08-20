503c8019bde34689deb116568ddd1694eba13032
Hend123 <henda4544@gmail.com>
2022-08-19 23:13:11 -0700

    add api key

--- app/src/main/java/com/udacity/asteroidradar/utils/Constants.kt
+++ app/src/main/java/com/udacity/asteroidradar/utils/Constants.kt
@@ -1,7 +1,7 @@
 package com.udacity.asteroidradar.utils
 
 object Constants {
-    const val API_KEY = "Demo"
+    const val API_KEY = "h8pgzzK8VKMdnZClhcooWVoEDJQez2ejikt7YK9m"
     const val API_QUERY_DATE_FORMAT = "yyyy-MM-dd"
     const val DEFAULT_END_DATE_DAYS = 7
     const val BASE_URL = "https://api.nasa.gov/"
